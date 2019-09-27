17d0b622743a0f628c0e42e8514f4a639f5ded3f
Archer <archer.huang@aceqilin.com>
2019-09-27 16:22:49 +0800

    Adding some Order Checks

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -12,14 +12,15 @@
                     <input type="number"
                            class="form-control"
                            placeholder="Quantity"
-                           v-model.number="quantity">
+                           v-model.number="quantity"
+                           :class="{danger: insufficientFunds}">
                 </div>
                 <div class="pull-right">
                     <button
                             class="btn btn-success"
                             @click="buyStock"
-                            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                    >Buy
+                            :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
+                    >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}
                     </button>
                 </div>
             </div>
@@ -27,12 +28,26 @@
     </div>
 </template>
 
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
+
 <script>
     export default {
         props: ['stock'],
         data() {
             return {
                 quantity: 0
+            }
+        },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
             }
         },
         methods: {

9404fa696488d44baceff2f2d8620dad24520b51
Archer <archer.huang@aceqilin.com>
2019-09-27 17:44:46 +0800

    Saving Data (PUT Request)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,7 +23,7 @@
                            aria-expanded="false">Save & Load <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save Data</a></li>
+                            <li><a @click="saveData" style="cursor: pointer">Save Data</a></li>
                             <li><a href="#">Load Data</a></li>
                         </ul>
                     </li>
@@ -53,6 +53,14 @@
             ]),
             endDay() {
                 this.randomizeStocks();
+            },
+            saveData() {
+                const data = {
+                    funds: this.$store.getters.funds,
+                    stockPortfolio: this.$store.getters.stockPortfolio,
+                    stocks: this.$store.getters.stocks
+                };
+                this.$http.put('data.json', data);
             }
         }
     }
